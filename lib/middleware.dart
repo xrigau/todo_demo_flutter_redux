42da8f5bf10d06ea83ae0f821546ffc7fa4a0d29
Xavi Rigau <xavi@novoda.com>
2018-04-02 14:32:01 +0200

    Update redux & flutter_redux to their latest versions
    These have support for Dart 2.0 :tada: which means more type safety

--- lib/middleware.dart
+++ lib/middleware.dart
@@ -1,16 +1,14 @@
 import 'dart:async';
 
-import 'package:todo_demo_flutter_redux/actions.dart';
 import 'package:redux/redux.dart';
+import 'package:todo_demo_flutter_redux/actions.dart';
 import 'package:todo_demo_flutter_redux/state.dart';
 
-List<Middleware<AppState>> createStoreMiddleware() => combineTypedMiddleware([
-      new MiddlewareBinding<AppState, SaveListAction>(_saveList),
-    ]);
+List<Middleware<AppState>> createStoreMiddleware() => [
+      TypedMiddleware<AppState, SaveListAction>(_saveList),
+    ];
 
-Future _saveList(Store<AppState> store, action, NextDispatcher next) async {
-  if (action is SaveListAction) {
-    await Future.sync(() => Duration(seconds: 3)); // Simulate saving the list to disk
-    next(action);
-  }
+Future _saveList(Store<AppState> store, SaveListAction action, NextDispatcher next) async {
+  await Future.sync(() => Duration(seconds: 3)); // Simulate saving the list to disk
+  next(action);
 }
--- lib/reducers.dart
+++ lib/reducers.dart
@@ -5,30 +5,20 @@ import 'package:todo_demo_flutter_redux/actions.dart';
 
 AppState appReducer(AppState state, action) => AppState(toDoListReducer(state.toDos, action), listStateReducer(state.listState, action));
 
-final toDoListReducer = combineTypedReducers<List<ToDoItem>>([
-  ReducerBinding<List<ToDoItem>, AddItemAction>((List<ToDoItem> toDos, dynamic action) => _fixTypes<List<ToDoItem>, AddItemAction>(toDos, action, _addItem)),
-  ReducerBinding<List<ToDoItem>, RemoveItemAction>((List<ToDoItem> toDos, dynamic action) => _fixTypes<List<ToDoItem>, RemoveItemAction>(toDos, action, _removeItem)),
+final Reducer<List<ToDoItem>> toDoListReducer = combineReducers([
+  TypedReducer<List<ToDoItem>, AddItemAction>(_addItem),
+  TypedReducer<List<ToDoItem>, RemoveItemAction>(_removeItem),
 ]);
 
-List<ToDoItem> _removeItem(List<ToDoItem> toDos, RemoveItemAction action) {
-  List<ToDoItem> mutableList = List.from(toDos);
-  mutableList.remove(action.item);
-  return List.unmodifiable(mutableList);
-}
+List<ToDoItem> _removeItem(List<ToDoItem> toDos, RemoveItemAction action) => List.unmodifiable(List.from(toDos)..remove(action.item));
 
-List<ToDoItem> _addItem(List<ToDoItem> toDos, AddItemAction action) {
-  List<ToDoItem> mutableList = List.from(toDos);
-  mutableList.add(action.item);
-  return List.unmodifiable(mutableList);
-}
+List<ToDoItem> _addItem(List<ToDoItem> toDos, AddItemAction action) => List.unmodifiable(List.from(toDos)..add(action.item));
 
-final listStateReducer = combineTypedReducers<ListState>([
-  ReducerBinding<ListState, DisplayListOnlyAction>((ListState listState, dynamic action) => _fixTypes<ListState, DisplayListOnlyAction>(listState, action, _displayListOnly)),
-  ReducerBinding<ListState, DisplayListWithNewItemAction>((ListState listState, dynamic action) => _fixTypes<ListState, DisplayListWithNewItemAction>(listState, action, _displayListWithNewItem)),
+final Reducer<ListState> listStateReducer = combineReducers<ListState>([
+  TypedReducer<ListState, DisplayListOnlyAction>(_displayListOnly),
+  TypedReducer<ListState, DisplayListWithNewItemAction>(_displayListWithNewItem),
 ]);
 
 ListState _displayListOnly(ListState listState, DisplayListOnlyAction action) => ListState.listOnly;
 
 ListState _displayListWithNewItem(ListState listState, DisplayListWithNewItemAction action) => ListState.listWithNewItem;
-
-D _fixTypes<D, A>(D data, A action, Function reducer) => reducer(data, action);
